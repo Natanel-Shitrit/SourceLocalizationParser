476dd1b2e0fc5f14a771a9d15b83725cf0c440c3
Natanel Shitrit <natanelsht@gmail.com>
2022-01-01 16:15:38 +0200

    Add extension files

---
+++ extension.cpp
@@ -0,0 +1,33 @@
+#include "extension.h"
+
+LanguagePhrasesParser g_Extension;	
+SMEXT_LINK(&g_Extension);
+
+IFileSystem* g_pFileSystem = NULL;
+ILanguageFileParser* g_pLanguageFileParser = NULL;
+
+bool LanguagePhrasesParser::SDK_OnMetamodLoad(ISmmAPI* ismm, char* error, size_t maxlen, bool late)
+{
+	GET_V_IFACE_CURRENT(GetEngineFactory, g_pFileSystem, IFileSystem, FILESYSTEM_INTERFACE_VERSION);
+	return true;
+}
+
+void LanguagePhrasesParser::NotifyInterfaceDrop(SMInterface* pInterface)
+{
+	if (g_pLanguageFileParser == pInterface)
+	{
+		CPhrasesGenerator::SDK_OnUnload();
+		g_pLanguageFileParser = NULL;
+	}
+}
+
+void LanguagePhrasesParser::SDK_OnUnload()
+{
+	CPhrasesGenerator::SDK_OnUnload();
+}
+
+void LanguagePhrasesParser::SDK_OnAllLoaded()
+{
+	SM_GET_LATE_IFACE(ILANGPARSER, g_pLanguageFileParser);
+	g_Extension.Generate();
+}
---
+++ phrasesgen.h
@@ -0,0 +1,79 @@
+#ifndef _INCLUDE_PHRASESGEN_H_
+#define _INCLUDE_PHRASESGEN_H_
+
+#include "smsdk_ext.h"
+#include "ILanguageFileParser.h"
+
+#include <string>
+#include <unordered_set>
+#include <filesystem.h>
+#include <fstream>
+#include <atomic>
+#include <chrono>
+#include <vector>
+
+class CPhrasesGenerator :
+	public ILanguageFileParserListener,
+	public IThread
+{
+public:
+	CPhrasesGenerator():
+		m_bEnglishFile(false),
+		m_pszLangCode(NULL),
+		m_nSection(Section_None),
+		m_nParsed(0),
+		m_pThread(NULL),
+		m_bReqTerm(false)
+	{
+	}
+
+public:
+	void Generate();
+	void SDK_OnUnload();
+
+private:
+	enum Section_t
+	{
+		Section_None,
+		Section_Settings,
+		Section_Tokens,
+		Section_Unknown,
+	};
+
+	bool m_bEnglishFile;
+	const char* m_pszLangCode;
+	std::ofstream m_Out;
+
+	Section_t m_nSection;
+	size_t m_nParsed;
+
+public: // ILanguageFileParserListener
+	ParseAction_t State_EnteredSection(const char* pszKey) override;
+	ParseAction_t State_KeyValue(const char* pszKey, const char* pszValue) override;
+	void State_Ended(bool halted, bool failed) override;
+
+private:
+	IThreadHandle* m_pThread;
+	std::atomic<bool> m_bReqTerm;
+	std::chrono::high_resolution_clock::time_point m_tmBegin;
+
+	void ParseTokensFromFile(const char* pszBase, const char* pszLanguage);
+
+public: // IThread
+	/**
+	 * @brief Called when the thread runs (in its own thread).
+	 *
+	 * @param pHandle		Pointer to the thread's handle.
+	 */
+	void RunThread(IThreadHandle* pHandle) override;
+
+	/**
+	 * @brief Called when the thread terminates.  This occurs inside the thread as well.
+	 *
+	 * @param pHandle		Pointer to the thread's handle.
+	 * @param cancel		True if the thread did not finish, false otherwise.
+	 */
+	void OnTerminate(IThreadHandle* pHandle, bool cancel) override;
+};
+
+#endif // _INCLUDE_PHRASESGEN_H_
