df19229c1d7d01a7ef6b13d58e0935417dd70f1b
Natanel Shitrit <natanelsht@gmail.com>
2022-07-08 18:29:02 +0300

    Bump version and correct description

--- src/ext/smsdk_config.h
+++ src/ext/smsdk_config.h
@@ -8,8 +8,8 @@
 
 /* Basic information exposed publicly */
 #define SMEXT_CONF_NAME			"SourceLocalizationParser"
-#define SMEXT_CONF_DESCRIPTION	"Generates SourceMod translations files from Source-Engine localization files."
-#define SMEXT_CONF_VERSION		"1.0.0.0"
+#define SMEXT_CONF_DESCRIPTION	"Generates SourceMod translations phrases files from Source-Engine localization files."
+#define SMEXT_CONF_VERSION		"2.0.0.0"
 #define SMEXT_CONF_AUTHOR		"LuqS"
 #define SMEXT_CONF_URL			"https://github.com/Natanel-Shitrit/"
 #define SMEXT_CONF_LOGTAG		"SLP"
